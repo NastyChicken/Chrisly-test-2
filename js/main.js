c4eec3350a40fabdfdbdd5cc75629b87f6c92348
Nattinger <nasim.attinger@bluewin.ch>
2018-04-09 11:48:47 +0200

    beta v1
    beta v1

---
+++ js/main.js
@@ -0,0 +1,209 @@
+/** 
+ * ===================================================================
+ * Main js
+ *
+ * ------------------------------------------------------------------- 
+ */ 
+
+(function($) {
+
+	"use strict";
+
+	/* --------------------------------------------------- */
+	/* Preloader
+	------------------------------------------------------ */ 
+   $(window).load(function() {
+      // will first fade out the loading animation 
+    	$("#loader").fadeOut("slow", function(){
+
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+
+      }); 
+  	})
+
+
+  	/*---------------------------------------------------- */
+	/* FitVids
+	------------------------------------------------------ */ 
+  	$(".fluid-video-wrapper").fitVids();
+
+
+	/* --------------------------------------------------- */
+	/*  Vegas Slideshow
+	------------------------------------------------------ */
+	$(".home-slides").vegas({
+		transition: 'fade',
+		transitionDuration: 2500,
+		delay: 5000,
+    	slides: [
+       	{ src: "images/slides/03.jpg" },
+        	{ src: "images/slides/02.jpg" },
+        	{ src: "images/slides/01.jpg" }
+    	]
+	});
+
+
+	/* --------------------------------------------------- */
+	/*  Particle JS
+	------------------------------------------------------ */
+	$('.home-particles').particleground({
+	   dotColor: '#fff',
+	   lineColor: '#555555',
+	   particleRadius: 6,
+	   curveLines: true,
+	   density: 10000,
+	   proximity: 110
+	}); 
+
+
+	/*-----------------------------------------------------*/
+	/* tabs
+  	-------------------------------------------------------*/	
+	$(".tab-content").hide();
+	$(".tab-content").first().show();
+
+	$("ul.tabs li").click(function () {
+	   $("ul.tabs li").removeClass("active");
+	   $(this).addClass("active");
+	   $(".tab-content").hide();
+	   var activeTab = $(this).attr("data-id");
+	   $("#" + activeTab).fadeIn(700);
+	});
+
+
+	/*----------------------------------------------------*/
+  	/* Smooth Scrolling
+  	------------------------------------------------------*/
+  	$('.smoothscroll').on('click', function (e) {
+	 	
+	 	e.preventDefault();
+
+   	var target = this.hash,
+    	$target = $(target);
+
+    	$('html, body').stop().animate({
+       	'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+      	window.location.hash = target;
+      });
+
+  	});
+
+
+  	/* --------------------------------------------------- */
+	/*  Placeholder Plugin Settings
+	------------------------------------------------------ */
+	$('input, textarea, select').placeholder()  
+
+
+  	/*---------------------------------------------------- */
+   /* ajaxchimp
+	------------------------------------------------------ */
+
+	// Example MailChimp url: http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
+	var mailChimpURL = 'http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
+
+	$('#mc-form').ajaxChimp({
+
+		language: 'es',
+	   url: mailChimpURL
+
+	});
+
+	// Mailchimp translation
+	//
+	//  Defaults:
+	//	 'submit': 'Submitting...',
+	//  0: 'Wir haben ihnen eine bestätigungs email geschickt',
+	//  1: 'Bitte eingeben ',
+	//  2: 'Eine Email Adresse muss ein @ beinhalten',
+	//  3: 'Die Domain ist falsch)',
+	//  4: 'The username portion of the email address is invalid (the portion before the @: )',
+	//  5: 'This email address looks fake or invalid. Please enter a real email address'
+
+	$.ajaxChimp.translations.es = {
+	  'Senden': 'Wird gesendet',
+	  0: '<i class="fa fa-check"></i> Wir haben ihnen eine bestätigungs email geschickt',
+	  1: '<i class="fa fa-warning"></i> Sie müssen eine echte Email Adresse eingeben',
+	  2: '<i class="fa fa-warning"></i> Email Adresse ist nicht gültig.',
+	  3: '<i class="fa fa-warning"></i> Email Adresse ist nicht gültig.',
+	  4: '<i class="fa fa-warning"></i> Email Adresse ist nicht gültig.',
+	  5: '<i class="fa fa-warning"></i> Email Adresse ist nicht gültig.'
+	}
+
+
+	/*---------------------------------------------------- */
+	/*	contact form
+	------------------------------------------------------ */
+
+	/* local validation */
+	$('#contactForm').validate({
+
+		/* submit via ajax */
+		submitHandler: function(form) {
+
+			var sLoader = $('#submit-loader');
+
+			$.ajax({      	
+
+		      type: "Senden",
+		      url: "inc/sendEmail.php",
+		      data: $(form).serialize(),
+		      beforeSend: function() { 
+
+		      	sLoader.fadeIn(); 
+
+		      },
+		      success: function(msg) {
+
+	            // Nachricht wurde gesendet 
+	            if (msg == 'OK') {
+	            	sLoader.fadeOut(); 
+	               $('#message-warning').hide();
+	               $('#contactForm').fadeOut();
+	               $('#message-success').fadeIn();   
+	            }
+	            // Es gab ein Fehler
+	            else {
+	            	sLoader.fadeOut(); 
+	               $('#message-warning').html(msg);
+		            $('#message-warning').fadeIn();
+	            }
+
+		      },
+		      error: function() {
+
+		      	sLoader.fadeOut(); 
+		      	$('#message-warning').html("Something went wrong. Please try again.");
+		         $('#message-warning').fadeIn();
+
+		      }
+
+	      });     		
+  		}
+
+	});
+
+
+	/*----------------------------------------------------*/
+	/* Final Countdown Settings
+	------------------------------------------------------ */
+	var finalDate = '2018/01/01';
+
+	$('div#counter').countdown(finalDate)
+   	.on('update.countdown', function(event) {
+
+   		$(this).html(event.strftime('<div class=\"half\">' +
+   											 '<span>%D <sup>days</sup></span>' + 
+   										 	 '<span>%H <sup>hours</sup></span>' + 
+   										 	 '</div>' +
+   										 	 '<div class=\"half\">' +
+   										 	 '<span>%M <sup>mins</sup></span>' +
+   										 	 '<span>%S <sup>secs</sup></span>' +
+   										 	 '</div>'));
+
+   });     
+ 
+
+})(jQuery);
